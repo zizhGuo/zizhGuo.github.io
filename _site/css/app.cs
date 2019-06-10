f598cba51970bc278a284263575f9f863bcf1878
Zizhun Guo <zg2808@g.rit.edu>
2019-06-09 23:20:12 -0400

    initial version with basic layout function worked fine

---
+++ _site/css/app.cs
@@ -0,0 +1,92 @@
+		body {
+			/*Color need to be changed!*/
+			/*background: #212529;*/
+			background: white;
+			/*background: white;*/
+			font-family: "Nunito", sans-serif;
+		}
+
+		#mainNavbar {
+			font-size: 1.5rem;
+			font-weight: 100;
+			background: rgba(33,37,41, 1);
+		}
+
+		#mainNavbar .nav-link {
+			color: white;
+			font-size: 1rem;
+		}
+
+		#mainNavbar .nav-link:hover {
+			/*Color need to be changed!*/
+			color: #EA1C2C;
+		}
+
+		#mainNavbar .navbar-brand {
+			color: #EA1C2C;
+			font-size: 1.5rem;
+		}
+
+		.navbar.scrolled {
+   			background: rgba(33,37,41, 1);
+    		transition: background 500ms;
+		}
+		.navbar-toggler {
+			/*Color need to be changed!*/
+			background: #EA1C2C;
+		}
+
+		.container {
+			width: 50%;
+		}
+
+		#profile-image {
+			size: 10px;
+			width: 10rem;
+		}
+
+		.card {
+			/*background: #212529;*/
+			margin-top: 2rem;
+			margin-bottom: 2rem;
+		}
+
+		.center-cropped {
+  			width: 20em;
+  			height: 10em;
+  			background-position: center center;
+  			background-repeat: no-repeat;
+			}
+
+
+		#sidebar-pic {
+			width: auto;
+			height: 100%;
+			position:fixed;
+		}
+
+		.card {
+			border: none;
+			margin-top: 0;
+			margin-bottom: 2rem;
+		}
+		.container-fluid {
+			margin-left: 10rem;
+			height: 100%;
+		}
+
+		 .nav-tabs  {
+    		background-color:white;
+    		font-weight:bolder;
+    		border-top-left-radius:10px;
+    		border-top-right-radius:10px;
+    		margin-top: 20px;
+    		color:orange;
+		}
+		a {
+			color: black;
+		}
+
+		.nav-bar a {
+			color: black;
+		}
---
+++ thoughts.md
@@ -0,0 +1,5 @@
+---
+layout: page
+title: Thoughts
+img-bg: chengdu2.jpg
+---
